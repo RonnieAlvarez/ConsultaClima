ff08f1dc962c27ed4e5c3b9384a8525e9622ec6a
RonnieAlvarez <ronnie.alvarezc@gmail.com>
2023-08-29 22:07:30 -0600

    inicial

---
+++ src/components/AppClima.jsx
@@ -0,0 +1,16 @@
+import Formulario from "./Formulario"
+import Resultado from './Resultado';
+
+function AppClima() {
+  return (
+    <>
+      <main className="dos-columnas">
+        <Formulario />
+        
+        <Resultado />
+      </main>
+    </>
+  )
+}
+
+export default AppClima
---
+++ src/components/Resultado.jsx
@@ -0,0 +1,10 @@
+
+
+
+const Resultado = () => {
+  return (
+    <div>Resultado</div>
+  )
+}
+
+export default Resultado
